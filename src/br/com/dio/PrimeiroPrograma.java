074713d2189202c9ad39231805eea3c5d6b2ca0a
Pablo <cipolatti@gmail.com>
2021-08-11 12:18:47 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato("Nina", "Branca e Preto", 12);
+		System.out.println(gato);
+
+	}
+
+}
